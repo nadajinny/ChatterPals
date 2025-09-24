d5c65aeed1a1a0597bc6aa9b88b649d77651971c
NADAisWATCHING <145518496+nadajinny@users.noreply.github.com>
2025-09-25 00:01:08 +0900

    add mypage.vue & link with header

--- frontend-app/src/router/index.ts
+++ frontend-app/src/router/index.ts
@@ -4,6 +4,7 @@ import LevelTest from '../views/LevelTest.vue'
 import AITutor from '../views/AITutor.vue'
 import StudyLog from '../views/StudyLog.vue'
 import Settings from '../views/Settings.vue'
+import MyPage from '../views/MyPage.vue'
 const routes = [
   {
     path: '/',     
@@ -29,6 +30,11 @@ const routes = [
     path: '/settings', 
     name: 'Settings', 
     component: Settings
+  }, 
+  {
+    path: '/mypage', 
+    name: 'MyPage', 
+    component: MyPage
   }
 ]
 
